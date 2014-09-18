8212f1d66a02834d15d7e9386cbf377adcbcde87
Joy Pratt <joympratt@gmail.com>
2014-09-18 14:08:17 -0500

    working on show contact

--- client/views/show/show.js
+++ client/views/show/show.js
@@ -4,21 +4,9 @@
   angular.module('intouch')
   .controller('ShowCtrl', ['$scope', 'Show', 'Contact', function($scope, Show, Contact){
 
-    $scope.contact = {};
-
-    Contact.findById(this._id).then(function(response){
-      $scope.contact = response.data.contact;
-    });
-
     $scope.toggleContact = function(){
       $scope.showContact = !!!$scope.showContact;
     };
-
-    Contact.findById().then(function(response){
-      console.log('response in v/s/show.js>>>>>>>>>>>', response);
-      $scope.contact = response.data.contact;
-    });
-
 
     $scope.updateContact = function(){
       Contact.update($scope.contact).then(function(response){
