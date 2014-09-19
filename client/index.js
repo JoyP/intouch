c88a68a712fa3edc3c0227ffb6adf4dfbe1dc5da
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-09-19 10:19:17 -0500

    Login Auth

--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@
     .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
     .when('/contacts', {templateUrl:'/views/contacts/contacts.html', controller:'ContactsCtrl'})
     .when('/contacts/:contactId', {templateUrl:'/views/show/show.html', controller:'ShowCtrl'})
+    .when('/login', {templateUrl:'/views/login/login.html', controller:'LoginCtrl'})
     .otherwise({redirectTo:'/'});
 
     $httpProvider.interceptors.push('HttpInterceptor');
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var bcrypt = require('bcrypt');
-    //Mongo  = require('mongodb'),
+var bcrypt = require('bcrypt'),
+    Mongo  = require('mongodb');
     //_      = require('underscore-contrib');
 
 function User(){
@@ -10,6 +10,11 @@ function User(){
 Object.defineProperty(User, 'collection',{
   get: function(){return global.mongodb.collection('users');}
 });
+
+User.findById = function(id, cb){
+  var _id = Mongo.ObjectID(id);
+  User.collection.findOne({_id:_id}, cb);
+};
 
 User.loginUser = function(o, cb){
   User.collection.findOne({email:o.email}, function(err, user){
