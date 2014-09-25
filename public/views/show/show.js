03de64b1138edaf01e1afd8d9ce815000d0af2e0
Joy Pratt <joympratt@gmail.com>
2014-09-25 09:03:40 -0500

    to pull changes down

--- public/views/show/show.js
+++ public/views/show/show.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('intouch')
-  .controller('ShowCtrl', ['$scope', 'Show', 'Contact', '$routeParams', function($scope, Show, Contact, $routeParams){
+  .controller('ShowCtrl', ['$scope', '$location', 'Show', 'Contact', '$routeParams', function($scope, $location, Show, Contact, $routeParams){
 
     $scope.toggleContact = function(){
       $scope.showContact = !!!$scope.showContact;
@@ -13,8 +13,17 @@
         $scope.toggleContact();
       });
     };
+
     Show.findById($routeParams.contactId).then(function(response){
       $scope.contact = response.data.contact;
     });
+
+    $scope.deleteContact = function(contactId){
+      console.log('contactId in show controller>>>>>', contactId);
+      Show.deleteContact(contactId).then(function(response){
+        $location.path('/contacts');
+      });
+    };
+
   }]);
 })();
